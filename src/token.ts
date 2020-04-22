ba439e0fb77329835c5415f130e8f28b120b7333
Elad Perets <elad.perets@gmail.com>
2020-04-21 17:27:41 -0700

    Code editor
    Use an ACE code editor in the SPA.
    Also made a minor bugfix and linter fixes

--- src/token.ts
+++ src/token.ts
@@ -24,6 +24,7 @@ export enum TokenType {
 
 enum State {
     GetCommandOrEOF,
+    SkipToCommandOrEOF,
     GetCommandName,
     GetArgumentsOrSpaceBeforeExpressionValue,
     SeekNextArgument,
@@ -44,132 +45,152 @@ export class TokenizedExpression {
         this.tokens.push(token);
     }
 
-    public toExpressionDefinitions(): ParsedExpressionDefinitions {
+    public toExpressionDefinitions(isOptimistic = false): ParsedExpressionDefinitions {
         let state = State.GetCommandOrEOF;
         const expressionDefinitions: ExpressionDefinition[] = [];
         let currentExpression: ExpressionDefinition;
 
         // Remove all comments and leading\trailing whitespaces from the token stream. This can be done as part of the state machine, but it'll make things more complicated
         for (const tok of TokenizedExpression.trimWhitespacesAndRemoveComments(this.tokens)) {
-            switch (state) {
-                case State.GetCommandOrEOF:
-                    if (tok.type == TokenType.CmdSpecifier) {
-                        state = State.GetCommandName;
-                    } else if (tok.type != TokenType.EOF) {
-                        throw new Error("Expected '#' or EOF but found '" + tok.value + "' instead");
-                    }
-
-                    break;
-
-                case State.GetCommandName:
-                    if (tok.type == TokenType.Value || tok.type == TokenType.PotentialReference) {
-                        switch (tok.value) {
-                            case "def":
-                                currentExpression = new ExpressionDefinition();
-                                currentExpression.cmd = tok.value;
-                                currentExpression.tokenizedExpression = new TokenizedExpression();
-                                currentExpression.args = [];
-                                state = State.GetExpressionName;
-                                break;
-
-                            case "eval":
-                                currentExpression = new ExpressionDefinition();
-                                currentExpression.cmd = tok.value;
-                                currentExpression.tokenizedExpression = new TokenizedExpression();
-                                currentExpression.args = [];
-                                state = State.GetSpaceBeforeExpressionValue;
-                                break;
-
-                            default:
-                                throw new Error("Enountered unsupported command: '" + tok.value + "'");
-                        }
-                    } else {
-                        throw new Error("Expected command name but found '" + tok.value + "' instead");
-                    }
-
-                    break;
-
-                case State.GetExpressionName:
-                    if (tok.type == TokenType.Whitespace) {
-                        state = State.GetExpressionName;
-                    } else if (tok.type == TokenType.PotentialReference) {
-                        currentExpression.name = tok.value;
-                        state = State.GetArgumentsOrSpaceBeforeExpressionValue;
-                    } else {
-                        throw new Error("Expected legal expression name but found '" + tok.value + "' instead");
-                    }
-
-                    break;
-
-                case State.GetArgumentsOrSpaceBeforeExpressionValue:
-                    // If the expression name is followed by a whitespace, expect the expression value. Otherwise, it should have a '(', indicating the expression accepts arguments
-                    if (tok.type == TokenType.Whitespace) {
-                        state = State.AccumulateExpressionValue;
-                    } else if (tok.type == TokenType.OpeningParenthesis) {
-                        state = State.SeekNextArgument;
-                    } else {
-                        throw new Error("Expected whitespace or '(' but found '" + tok.value + "' instead");
-                    }
-
-                    break;
-
-                case State.SeekNextArgument:
-                    if (tok.type == TokenType.Whitespace) {
-                        state = State.SeekNextArgument;
-                    } else if (tok.type == TokenType.PotentialReference) {
-                        currentExpression.args.push(tok.value);
-                        state = State.ExpectedCommaOrClosingParenthesis;
-                    } else {
-                        throw new Error("Expected argument name but found '" + tok.value + "' instead");
-                    }
-
-                    break;
-
-                case State.ExpectedCommaOrClosingParenthesis:
-                    if (tok.type == TokenType.Comma) {
-                        state = State.SeekNextArgument;
-                    } else if (tok.type == TokenType.ClosingParenthesis) {
-                        state = State.GetSpaceBeforeExpressionValue;
-                    } else {
-                        throw new Error("Expected ')' or ',' but found '" + tok.value + "' instead");
-                    }
-
-                    break;
-                case State.GetSpaceBeforeExpressionValue:
-                    if (tok.type == TokenType.Whitespace) {
-                        state = State.AccumulateExpressionValue;
-                    }
-                    else {
-                        throw new Error("Expected whitespace but found '" + tok.value + "' instead");
-                    }
-
-                    break;
-
-                case State.AccumulateExpressionValue:
-                    switch (tok.type) {
-                        case TokenType.Value:
-                        case TokenType.PotentialReference:
-                        case TokenType.Whitespace:
-                        case TokenType.OpeningParenthesis:
-                        case TokenType.ClosingParenthesis:
-                        case TokenType.Comma:
-                            currentExpression.tokenizedExpression.push(tok);
-                            state = State.AccumulateExpressionValue;
-                            break;
-
-                        case TokenType.EOF:
-                            currentExpression.tokenizedExpression.tokens = TokenizedExpression.trimWhitespaces(currentExpression.tokenizedExpression.tokens);
-                            expressionDefinitions.push(currentExpression);
-                            break;
-
-                        case TokenType.CmdSpecifier:
-                            currentExpression.tokenizedExpression.tokens = TokenizedExpression.trimWhitespaces(currentExpression.tokenizedExpression.tokens);
-                            expressionDefinitions.push(currentExpression);
+            try {
+                switch (state) {
+                    case State.GetCommandOrEOF:
+                        if (tok.type == TokenType.CmdSpecifier) {
                             state = State.GetCommandName;
-                            break;
-                    }
+                        } else if (tok.type != TokenType.EOF) {
+                            throw new Error("Expected '#' or EOF but found '" + tok.value + "' instead");
+                        }
 
-                    break;
+                        break;
+
+                    case State.SkipToCommandOrEOF:
+                        if (!isOptimistic) throw new Error('GetCommandOrEOF can only be reached in optimistic mode');
+
+                        if (tok.type == TokenType.CmdSpecifier) {
+                            state = State.GetCommandName;
+                        } else {
+                            state = State.GetCommandOrEOF;
+                        }
+
+                        break;
+                    case State.GetCommandName:
+                        if (tok.type == TokenType.Value || tok.type == TokenType.PotentialReference) {
+                            switch (tok.value) {
+                                case "def":
+                                    currentExpression = new ExpressionDefinition();
+                                    currentExpression.cmd = tok.value;
+                                    currentExpression.tokenizedExpression = new TokenizedExpression();
+                                    currentExpression.args = [];
+                                    state = State.GetExpressionName;
+                                    break;
+
+                                case "eval":
+                                    currentExpression = new ExpressionDefinition();
+                                    currentExpression.cmd = tok.value;
+                                    currentExpression.tokenizedExpression = new TokenizedExpression();
+                                    currentExpression.args = [];
+                                    state = State.GetSpaceBeforeExpressionValue;
+                                    break;
+
+                                default:
+                                    throw new Error("Enountered unsupported command: '" + tok.value + "'");
+                            }
+                        } else {
+                            throw new Error("Expected command name but found '" + tok.value + "' instead");
+                        }
+
+                        break;
+
+                    case State.GetExpressionName:
+                        if (tok.type == TokenType.Whitespace) {
+                            state = State.GetExpressionName;
+                        } else if (tok.type == TokenType.PotentialReference) {
+                            currentExpression.name = tok.value;
+                            state = State.GetArgumentsOrSpaceBeforeExpressionValue;
+                        } else {
+                            throw new Error("Expected legal expression name but found '" + tok.value + "' instead");
+                        }
+
+                        break;
+
+                    case State.GetArgumentsOrSpaceBeforeExpressionValue:
+                        // If the expression name is followed by a whitespace, expect the expression value. Otherwise, it should have a '(', indicating the expression accepts arguments
+                        if (tok.type == TokenType.Whitespace) {
+                            state = State.AccumulateExpressionValue;
+                        } else if (tok.type == TokenType.OpeningParenthesis) {
+                            state = State.SeekNextArgument;
+                        } else {
+                            throw new Error("Expected whitespace or '(' but found '" + tok.value + "' instead");
+                        }
+
+                        break;
+
+                    case State.SeekNextArgument:
+                        if (tok.type == TokenType.Whitespace) {
+                            state = State.SeekNextArgument;
+                        } else if (tok.type == TokenType.PotentialReference) {
+                            currentExpression.args.push(tok.value);
+                            state = State.ExpectedCommaOrClosingParenthesis;
+                        } else {
+                            throw new Error("Expected argument name but found '" + tok.value + "' instead");
+                        }
+
+                        break;
+
+                    case State.ExpectedCommaOrClosingParenthesis:
+                        if (tok.type == TokenType.Whitespace) {
+                            state = State.ExpectedCommaOrClosingParenthesis;
+                        } else if (tok.type == TokenType.Comma) {
+                            state = State.SeekNextArgument;
+                        } else if (tok.type == TokenType.ClosingParenthesis) {
+                            state = State.GetSpaceBeforeExpressionValue;
+                        } else {
+                            throw new Error("Expected ')' or ',' but found '" + tok.value + "' instead");
+                        }
+
+                        break;
+                    case State.GetSpaceBeforeExpressionValue:
+                        if (tok.type == TokenType.Whitespace) {
+                            state = State.AccumulateExpressionValue;
+                        }
+                        else {
+                            throw new Error("Expected whitespace but found '" + tok.value + "' instead");
+                        }
+
+                        break;
+
+                    case State.AccumulateExpressionValue:
+                        switch (tok.type) {
+                            case TokenType.Value:
+                            case TokenType.PotentialReference:
+                            case TokenType.Whitespace:
+                            case TokenType.OpeningParenthesis:
+                            case TokenType.ClosingParenthesis:
+                            case TokenType.Comma:
+                                currentExpression.tokenizedExpression.push(tok);
+                                state = State.AccumulateExpressionValue;
+                                break;
+
+                            case TokenType.EOF:
+                                currentExpression.tokenizedExpression.tokens = TokenizedExpression.trimWhitespaces(currentExpression.tokenizedExpression.tokens);
+                                expressionDefinitions.push(currentExpression);
+                                break;
+
+                            case TokenType.CmdSpecifier:
+                                currentExpression.tokenizedExpression.tokens = TokenizedExpression.trimWhitespaces(currentExpression.tokenizedExpression.tokens);
+                                expressionDefinitions.push(currentExpression);
+                                state = State.GetCommandName;
+                                break;
+                        }
+
+                        break;
+                }
+            } catch (e) {
+                if (isOptimistic) {
+                    state = State.SkipToCommandOrEOF;
+                } else {
+                    throw e
+                }
             }
         }
 
@@ -192,9 +213,9 @@ export class TokenizedExpression {
     }
 
     public static trimWhitespacesAndRemoveComments(tokens: Token[]): Token[] {
-        let buf: Token[] = [];
+        const buf: Token[] = [];
         let inComment = false;
-        for (let token of tokens) {
+        for (const token of tokens) {
             if (token.type == TokenType.CommentSpecifier) {
                 inComment = true;
             } else if (inComment && token.type != TokenType.EOF) {
